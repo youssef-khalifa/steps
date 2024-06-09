818eae13be5fa1511ac1fd5bbe29ee6bdfe46a1a
youssef-khalifa <ykhalifa19921@gmail.com>
2024-06-10 00:00:35 +0300

    ز

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unreachable */
+// eslint-disable-next-line no-unused-vars
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+export default function App() {
+  let step = 2;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={` ${step===1? 'active' :''} `}>1</div>
+        <div className={` ${step===2? 'active' :''} `}>2</div>
+        <div className={` ${step===3? 'active' :''} `}>3</div>
+      </div>
+      <p className="message">Step {step}:{messages[step-1]}</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}onClick={()=>step--}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
